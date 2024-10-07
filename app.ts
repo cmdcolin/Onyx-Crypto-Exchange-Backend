735081e0255961c267863f3469f4c150ef577414
Bozhidar Kabzamalov <kabzamalov.bozhidar@gmail.com>
2024-10-07 23:25:52 +0300

    Add TypeScript, configure ESLint+Prettier and add initial Docker setup

--- app.ts
+++ app.ts
@@ -1,10 +1,10 @@
+var express = require('express');
 const createError = require('http-errors');
-const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const routes = require('./routes/routes');
+const routes = require('./routes/routes.ts');
 
 const app = express();
 
@@ -17,18 +17,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
+	// render the error page
+	res.status(err.status || 500);
 });
 
 module.exports = app;
